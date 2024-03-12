d1d26120c83dbd2a44ed8d30166fda549991b9a2
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-03-12 18:41:05 +1100

    setup pipeline as complexity increases

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -3,3 +3,9 @@ variable "env" {
   type        = string
   default     = "dev"
 }
+
+variable "id_file" {
+  description = "id file for ssh"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
