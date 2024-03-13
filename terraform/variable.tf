8cce19fbb2956577e1fd273546b5ebeb34ca64d8
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-03-13 23:28:31 +1100

    fix idfile

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,9 @@
 variable "env" {
   description = "which environment we are deploying the iac to"
   type        = string
-  default     = "dev"
 }
 
 variable "id_file" {
   description = "id file for ssh"
   type        = string
-  default     = "~/.ssh/id_rsa.pub"
 }
